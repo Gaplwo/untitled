51aab6ea0f19ed4f41be24d1940e5616c9aba4ba
ahmedg <elgebalia34@gmail.com>
2022-08-23 20:02:22 +0200

    Initial commit

---
+++ src/mm/mm.java
@@ -0,0 +1,15 @@
+package mm;
+import java.util.Scanner;
+public class mm {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Hello world!");
+        for (int i =0; i<=10 ; i++)
+        {
+            System.out.println("plase enter your number :");
+            i =  reader.nextInt();
+            System.out.println("ok \n");
+        }
+    }
+}
---
+++ src/ns.java
@@ -0,0 +1,5 @@
+package ns;
+import java.util.Scanner;
+
+public class ns {
+}
